43e827e55fe90e2759b1cd4422ffb0f111174beb
huy211994 <huy211994@gmail.com>
2019-10-24 23:07:17 -0700

    Updates

---
+++ precache-manifest.f60bb4a39715e103dbd3583b2d1865b9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ecb65f33eef0707786f52c2ff8722e37",
+    "url": "/test/index.html"
+  },
+  {
+    "revision": "3159ae8c4c24f2a731f6",
+    "url": "/test/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "7a5e435a97cd1f1b61ae",
+    "url": "/test/static/js/2.4a3d53c3.chunk.js"
+  },
+  {
+    "revision": "3159ae8c4c24f2a731f6",
+    "url": "/test/static/js/main.2600110c.chunk.js"
+  },
+  {
+    "revision": "d4478ee8f543320a5791",
+    "url": "/test/static/js/runtime-main.b6591697.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/test/static/media/logo.25bf045c.svg"
+  }
+]);
